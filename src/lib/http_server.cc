2986486d2e9a787eb809b574770dc73ad15b0607
Antonio de la Vega <antoniodlavga@gmail.com>
2019-05-28 23:48:17 +0100

    Use proper param types in accept.

--- src/lib/http_server.cc
+++ src/lib/http_server.cc
@@ -1,18 +1,14 @@
 #include <netinet/in.h>
-#include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 #include "http_server.h"
 
-HttpServer::HttpServer(/* args */)
-{
-}
+HttpServer::HttpServer(/* args */) {}
 
-HttpServer::~HttpServer()
-{
-}
+HttpServer::~HttpServer() {}
 
 void HttpServer::Listen(int port) {
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -26,27 +22,35 @@ void HttpServer::Listen(int port) {
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = port;
+  int addrlen = sizeof(address);
 
-  if (bind(sockfd, (struct sockaddr *) &address, sizeof(address)) < 0) {
+  int opt = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+    perror("setsockopt failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (bind(sockfd, (struct sockaddr *)&address, addrlen) < 0) {
     perror("bind failed");
     exit(EXIT_FAILURE);
   }
 
-  const int backlog = 3;
+  const int backlog = 1;
 
   if (listen(sockfd, backlog) < 0) {
     perror("listening failed");
     exit(EXIT_FAILURE);
   }
 
-  int new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t *) sizeof(address));
+  int new_socket =
+      accept(sockfd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
 
   if (new_socket < 0) {
     perror("accept failed");
     exit(EXIT_FAILURE);
   }
 
-  char buffer[1024] = {0}; 
+  char buffer[1024] = {0};
   read(new_socket, buffer, 1024);
-  printf("%s\n",buffer );
+  printf("%s\n", buffer);
 }
